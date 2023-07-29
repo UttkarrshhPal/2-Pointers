f795e4774d7a62c755df70c9e6e0734670fa0457
Utkarsh_Pal <110682756+UttkarrshhPal@users.noreply.github.com>
2023-07-29 19:17:58 +0530

    Add files via upload

---
+++ container_With_Most_Water.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int height[]={1,8,6,2,5,4,8,3,7};
+    int n = 9;
+    //while(first<last)
+    // first  = 1
+    // last   = 7
+    //min(first,last)=1
+    // capacity = min(first,last)*widht
+    // capacity = 8*1=8
+
+    // Now height should rise
+    // if(height[first]>height[last])
+    // last-=1;
+
+    // if(height[first]<height[last])
+    // first+=1;
+
+    
+}
